891291d9cfe9e773994be06339513fdcb9d72cb2
Marino2401 <159966620+Marino2401@users.noreply.github.com>
2024-03-05 00:21:24 +0000

    ver. 5

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
+#include <list>
+
+
+
+std::cout
 using namespace std;
 
 int main(int argc, char const *argv[])
 {
-    cout<<"hola"<<"23110347"<<endl;
-    
+    cout << "hola"
+         << "23110347" << endl;
+
     return 0;
 }
